34d653e89efee28b2ea3b5d4c594683b268a8569
uatrade <piskovtsev@gmail.com>
2018-08-11 12:03:58 +0300

    Add project files.

---
+++ UsersApp/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace UsersApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textlogin = new System.Windows.Forms.TextBox();
+            this.textPassw = new System.Windows.Forms.TextBox();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.textPhone = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboRole = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonGetList = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textlogin
+            // 
+            this.textlogin.Location = new System.Drawing.Point(106, 12);
+            this.textlogin.Multiline = true;
+            this.textlogin.Name = "textlogin";
+            this.textlogin.Size = new System.Drawing.Size(121, 20);
+            this.textlogin.TabIndex = 0;
+            // 
+            // textPassw
+            // 
+            this.textPassw.Location = new System.Drawing.Point(106, 51);
+            this.textPassw.Multiline = true;
+            this.textPassw.Name = "textPassw";
+            this.textPassw.Size = new System.Drawing.Size(121, 20);
+            this.textPassw.TabIndex = 1;
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(106, 89);
+            this.textEmail.Multiline = true;
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(121, 20);
+            this.textEmail.TabIndex = 2;
+            // 
+            // textPhone
+            // 
+            this.textPhone.Location = new System.Drawing.Point(106, 130);
+            this.textPhone.Multiline = true;
+            this.textPhone.Name = "textPhone";
+            this.textPhone.Size = new System.Drawing.Size(121, 20);
+            this.textPhone.TabIndex = 3;
+            this.textPhone.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Регистрация";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboRole
+            // 
+            this.comboRole.FormattingEnabled = true;
+            this.comboRole.Location = new System.Drawing.Point(106, 165);
+            this.comboRole.Name = "comboRole";
+            this.comboRole.Size = new System.Drawing.Size(121, 21);
+            this.comboRole.TabIndex = 5;
+            this.comboRole.SelectedIndexChanged += new System.EventHandler(this.comboRole_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Пароль";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Телефон";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Роль";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(334, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Список пользователей";
+            // 
+            // buttonGetList
+            // 
+            this.buttonGetList.Location = new System.Drawing.Point(483, 18);
+            this.buttonGetList.Name = "buttonGetList";
+            this.buttonGetList.Size = new System.Drawing.Size(140, 23);
+            this.buttonGetList.TabIndex = 13;
+            this.buttonGetList.Text = "Получить список";
+            this.buttonGetList.UseVisualStyleBackColor = true;
+            this.buttonGetList.Click += new System.EventHandler(this.buttonGetList_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(337, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(286, 259);
+            this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(483, 51);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(140, 23);
+            this.btnUpdate.TabIndex = 15;
+            this.btnUpdate.Text = "Обновить список";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 350);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonGetList);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboRole);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textPhone);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.textPassw);
+            this.Controls.Add(this.textlogin);
+            this.Name = "Form1";
+            this.Text = "Регистрация Пользователей";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textlogin;
+        private System.Windows.Forms.TextBox textPassw;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.TextBox textPhone;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboRole;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonGetList;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ UsersApp/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace UsersApp
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection conn;
+        private SqlDataReader reader;
+        SqlDataAdapter da = null;
+        DataSet set = null;
+        SqlCommandBuilder cmd = null;
+        string myrole;
+        string cs = "";
+        public Form1()
+        {
+            InitializeComponent();
+
+            conn = new SqlConnection();
+
+            cs= ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+            conn.ConnectionString = cs;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboRole.Items.Add("User");
+            comboRole.Items.Add("Admin");
+            comboRole.Text = comboRole.Items[0].ToString();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboRole_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            myrole = "";
+
+            myrole = comboRole.SelectedItem.ToString();
+        }
+
+        private void buttonGetList_Click(object sender, EventArgs e) //вывод списка
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "Select *from [UsersAdo].[dbo].Users";
+                // command.Connection = conn;
+                conn =new SqlConnection(cs);
+                set = new DataSet();
+                da = new SqlDataAdapter(command.CommandText, conn);
+                cmd = new SqlCommandBuilder(da);
+                dataGridView1.DataSource = null;      
+
+                da.Fill(set, "[UsersAdo].[dbo].Users");
+                dataGridView1.DataSource =set.Tables["[UsersAdo].[dbo].Users"];
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if(conn!=null)
+                conn.Close();
+                if (reader != null)
+                reader.Close();
+            }
+
+        }
+
+        public bool ChooseLogin(string login)     //проверка логина на повторение
+        {
+            List<string> mylogin = new List<string>();
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "Select logins from [UsersAdo].[dbo].Users";
+                command.Connection = conn;
+                conn.Open();
+                reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    mylogin.Add(reader["logins"].ToString());
+                }
+
+                foreach(var items in mylogin)
+                {
+                    if(items.ToString()==login)
+                    {
+                        return true;
+                    } 
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return true;
+            }
+            finally
+            {
+                if (conn != null)
+                    conn.Close();
+                if (reader != null)
+                    reader.Close();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)   //регистрация
+        {
+            if (ChooseLogin(textlogin.Text) == true)
+            {
+                MessageBox.Show("Login уже существует");
+            }
+            else
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand commandAdd = new SqlCommand();
+                    commandAdd.Connection = conn;
+
+                    commandAdd.CommandText = $"Insert into [UsersAdo].[dbo].Users values('{textlogin.Text}', '{textPassw.Text.GetHashCode()}', '{textEmail.Text}', '{textPhone.Text}', '{myrole}')";
+                    commandAdd.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+
+                }
+
+                finally
+                {
+                    if (conn != null)
+                        conn.Close();
+                }
+
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            da.Update(set, "[UsersAdo].[dbo].Users");
+        }
+    }
+}
